c93f992d2d4e7cf9acfc6b1db3ec9a4882a92682
wubw <wubw@pku.edu.cn>
2023-09-17 01:56:54 +0800

    test: chain forging

--- chain_test.go
+++ chain_test.go
@@ -19,10 +19,14 @@ func TestAddBlock(t *testing.T) {
 func TestValidateChain(t *testing.T) {
 	c := NewChain()
 	t.Logf("validate chain with only ancestor block result: %v", c.Validate())
+
 	b := NewBlock("transfer10Yuan", "")
 	b1 := NewBlock("transferAnother10Yuan", "")
 	c.AddBlockToChain(b)
 	c.AddBlockToChain(b1)
 	t.Log(c)
 	t.Logf("validate chain after add some blocks result: %v", c.Validate())
+
+	c.chain[1].data = "transfer100Yuan"
+	t.Logf("validate chain after forge the block result: %v", c.Validate())
 }
