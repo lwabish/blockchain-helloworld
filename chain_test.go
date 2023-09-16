ebbabe79adddbc08154f31d93d41dd1912c7fda2
wubw <wubw@pku.edu.cn>
2023-09-17 01:30:21 +0800

    test: chain methods

--- chain_test.go
+++ chain_test.go
@@ -6,3 +6,10 @@ func TestNewChain(t *testing.T) {
 	c := NewChain()
 	t.Log(c)
 }
+
+func TestAddBlock(t *testing.T) {
+	c := NewChain()
+	b := NewBlock("transfer10yuan", "")
+	c.AddBlockToChain(b)
+	t.Log(c)
+}
