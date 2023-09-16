22eada574aeee568d85aa965c8d7294fb6207195
wubw <wubw@pku.edu.cn>
2023-09-17 01:30:14 +0800

    feat: add chain methods

--- chain.go
+++ chain.go
@@ -8,6 +8,16 @@ func (c *Chain) bigBang() Block {
 	return NewBlock("ancestorBlock", "")
 }
 
+func (c *Chain) GetLatestBlock() Block {
+	return c.chain[len(c.chain)-1]
+}
+
+func (c *Chain) AddBlockToChain(b Block) {
+	b.previousHash = c.GetLatestBlock().hash
+	b.hash = b.computeHash()
+	c.chain = append(c.chain, b)
+}
+
 func NewChain() Chain {
 	c := Chain{
 		chain: []Block{},
