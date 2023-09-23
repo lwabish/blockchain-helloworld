129d5af18e255b79367165623f75a214bd175bdf
wubw <wubw@pku.edu.cn>
2023-09-24 02:01:09 +0800

    perf: log

--- chain.go
+++ chain.go
@@ -37,7 +37,7 @@ func (c *Chain) Validate() bool {
 			return false
 		}
 		if b.hash != b.computeHash() {
-			fmt.Println("chain forged")
+			fmt.Printf("chain forged: current hash %s, recomputed hash %s", b.hash, b.computeHash())
 			return false
 		}
 	}
