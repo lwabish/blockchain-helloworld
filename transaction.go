89129a5a2d09f19e6e0cf98f73b563d116720544
wubw <wubw@pku.edu.cn>
2023-09-24 02:49:37 +0800

    feat: constructor

--- transaction.go
+++ transaction.go
@@ -5,3 +5,11 @@ type Transaction struct {
 	to     string
 	amount int
 }
+
+func NewTransaction(from string, to string, amount int) Transaction {
+	return Transaction{
+		from:   from,
+		to:     to,
+		amount: amount,
+	}
+}
