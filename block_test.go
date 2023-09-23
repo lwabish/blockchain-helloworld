a8eb970b20cfce87c6f2a97d0af95eafbd8390de
wubw <wubw@pku.edu.cn>
2023-09-24 01:17:07 +0800

    test: mine

--- block_test.go
+++ block_test.go
@@ -1,8 +1,24 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestNewBlock(t *testing.T) {
-	b := NewBlock("WUBOWEN", "1234")
+	b := NewBlock("lwabish", "1234")
 	t.Log(b)
+}
+
+func TestGenerateAnswer(t *testing.T) {
+	b := NewBlock("lwabish", "1234")
+	t.Log(b.generateAnswer(3))
+}
+
+func TestStringCut(t *testing.T) {
+	t.Log("abcde"[:3])
+}
+
+func TestMine(t *testing.T) {
+	b := NewBlock("lwabish", "1234")
+	t.Logf("\nmine success: %s", b.mine(3))
 }
